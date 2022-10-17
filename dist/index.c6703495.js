6497e196b551ff8b5cc92cf80304890b9601890c
SureshSamarawarna <suresh.asan8@gmail.com>
2022-10-17 14:51:09 +0530

    second commit

--- dist/index.c6703495.js
+++ dist/index.c6703495.js
@@ -1,4 +1,23 @@
-const h1 = document.getElementById("h1");
-h1.style.backgroundColor = `red`;
+const tblStudent = document.getElementById("tbl-student");
+const btnNew = document.getElementById("btn-new");
+const btnSave = document.getElementById("btn-save");
+const btnClear = document.getElementById("btn-clear");
+const txtId = document.getElementById("txt-id");
+const txtName = document.getElementById("txt-name");
+const txtConatct = document.getElementById("txt-contact");
+const txtAddress = document.getElementById("txt-address");
+btnNew.addEventListener("click", ()=>{
+    [
+        txtId,
+        txtName,
+        txtConatct,
+        txtAddress
+    ].forEach((ctrl)=>ctrl.disabled = false);
+    txtId.value = generateNewStudentId();
+    txtName.focus();
+});
+function generateNewStudentId() {
+    return "S001";
+}
 
 //# sourceMappingURL=index.c6703495.js.map
---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const tblStudent = document.getElementById('tbl-student');
+const btnNew = document.getElementById('btn-new');
+const btnSave = document.getElementById('btn-save');
+const btnClear = document.getElementById('btn-clear');
+const txtId = document.getElementById('txt-id');
+const txtName = document.getElementById('txt-name');
+const txtConatct = document.getElementById('txt-contact');
+const txtAddress = document.getElementById('txt-address');
+
+
+btnNew.addEventListener('click',()=>{
+    [txtId,txtName,txtConatct,txtAddress,btnSave,btnClear].forEach(ctrl => ctrl.disabled = false);
+    txtId.value = generateNewStudentId();
+    txtName.focus();
+});
+
+function generateNewStudentId(){
+    return "S001";
+}
